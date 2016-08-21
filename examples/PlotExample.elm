8773e17a70b1966bade49a63f39dbfce02d44d1f
Tereza Sokol <terezasokol@gmail.com>
2016-08-21 15:20:50 +0200

    spit into module

--- examples/PlotExample.elm
+++ examples/PlotExample.elm
@@ -1,29 +1,14 @@
 module PlotExample exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Debug
-
-type alias SerieConfig data =
-    { color : String
-    , areaColor : String
-    , toX : data -> List Int
-    , toY : data -> List Int
-    }
-
-
-type alias PlotConfig data =
-  { name: String
-  , height : Int
-  , width : Int
-  , series : List (SerieConfig data)
-  }
-
+import Plot
 
 main =
-  viewPlot plotConfig dataHere
+  Plot.viewPlot plotConfig dataHere
+
 
 type alias SeriesData =
   { x : List Int, y : List Int }
+
 
 dataHere : List SeriesData
 dataHere =
@@ -32,49 +17,13 @@ dataHere =
   ]
 
 
-plotConfig : PlotConfig SeriesData
+plotConfig : Plot.PlotConfig SeriesData
 plotConfig =
-  PlotConfig "Great plot" 200 400 serieConfigs
+  Plot.PlotConfig "Great plot" 200 400 serieConfigs
 
 
-serieConfigs : List (SerieConfig SeriesData)
+serieConfigs : List (Plot.SerieConfig SeriesData)
 serieConfigs =
-  [ SerieConfig "red" "magneta" .x .y
-  , SerieConfig "blue" "ariel" .x .y
+  [ Plot.SerieConfig "red" "magneta" .x .y
+  , Plot.SerieConfig "blue" "ariel" .x .y
   ]
-
-
-viewPlot : PlotConfig data -> List data -> Html msg
-viewPlot { name, series } data =
-  let
-    highestX = maxValue .toX series data
-    highestY = maxValue .toY series data
-  in
-    div []
-      [ div [] [ text name ]
-      , div [] [ text (toString highestX) ]
-      , div [] (List.map2 viewSerie series data)
-      ]
-
-
-viewSerie : SerieConfig data -> data -> Html msg
-viewSerie { color, toX, toY } data =
-  div []
-    [ div [] [ text (viewValues (toX data)) ]
-    , div [] [ text (viewValues (toY data)) ]
-    ]
-
-
-viewValues : List Int -> String
-viewValues values =
-  List.head values
-    |> Maybe.withDefault 0
-    |> toString
-
-
-maxValue : (SerieConfig data -> data -> List Int) -> List (SerieConfig data) -> List data -> Int
-maxValue toValues series data =
-  List.map2 toValues series data
-    |> List.concat
-    |> List.maximum
-    |> Maybe.withDefault 0
