d098936027d287e9caf932a1831b16944f1ecba8
Tereza Sokol <terezasokol@gmail.com>
2016-10-10 18:36:11 +0200

    Format

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -7,74 +7,80 @@ import String
 
 modulus : Float -> Float -> Float
 modulus divider value =
-  value - (toFloat (round (value / divider)))
+    value - (toFloat (round (value / divider)))
 
 
 byPrecision : Float -> (Float -> Int) -> Float -> Float
 byPrecision precision operation value =
-  toFloat (operation (value / precision)) * precision
+    toFloat (operation (value / precision)) * precision
 
 
 getHighest : List Float -> Float
 getHighest values =
-  Maybe.withDefault 1 (List.maximum values)
+    Maybe.withDefault 1 (List.maximum values)
 
 
 getLowest : List Float -> Float
 getLowest values =
-  min 0 (Maybe.withDefault 0 (List.minimum values))
+    min 0 (Maybe.withDefault 0 (List.minimum values))
 
 
-viewSvgContainer : (Float, Float) -> List (Svg.Svg a) -> Svg.Svg a
-viewSvgContainer (x, y) children =
-  Svg.g [ transform (toTranslate x y)] children
+viewSvgContainer : ( Float, Float ) -> List (Svg.Svg a) -> Svg.Svg a
+viewSvgContainer ( x, y ) children =
+    Svg.g [ transform (toTranslate x y) ] children
 
 
 viewAxisPath : String -> Svg.Svg a
 viewAxisPath path =
-  Svg.path [ d ("M0.5, 0" ++ path), style "stroke: #757575;" ] []
+    Svg.path [ d ("M0.5, 0" ++ path), style "stroke: #757575;" ] []
 
 
-viewSvgLine : (Float, Float, Int, Int) -> Svg.Svg a
-viewSvgLine (x1', y1', x2', y2') =
-  viewSvgContainer (x1', y1')
-    [ Svg.line [ x2 (toString x2'), y2 (toString y2'),  style "stroke: #757575;" ] [] ]
+viewSvgLine : ( Float, Float, Int, Int ) -> Svg.Svg a
+viewSvgLine ( x1', y1', x2', y2' ) =
+    viewSvgContainer ( x1', y1' )
+        [ Svg.line [ x2 (toString x2'), y2 (toString y2'), style "stroke: #757575;" ] [] ]
+
 
 
 -- text-anchor for yAxis should be end
-viewSvgText : (Float, Float, Int, Int) -> String -> Svg.Svg a
-viewSvgText (x, y, _, _) label =
-  viewSvgContainer (x, y)
-    [ Svg.text'
-      ([ style "stroke: #757575; text-anchor: middle;" ])
-      [ Svg.tspan [] [ Svg.text label ] ]
-    ]
+
+
+viewSvgText : ( Float, Float, Int, Int ) -> String -> Svg.Svg a
+viewSvgText ( x, y, _, _ ) label =
+    viewSvgContainer ( x, y )
+        [ Svg.text'
+            ([ style "stroke: #757575; text-anchor: middle;" ])
+            [ Svg.tspan [] [ Svg.text label ] ]
+        ]
 
 
 toTranslate : Float -> Float -> String
 toTranslate x y =
-   "translate(" ++ (toString x) ++ ", " ++ (toString y) ++ ")"
+    "translate(" ++ (toString x) ++ ", " ++ (toString y) ++ ")"
 
 
-coordToInstruction : String -> List (Float, Float) -> String
+coordToInstruction : String -> List ( Float, Float ) -> String
 coordToInstruction instructionType coords =
-  List.map (\(x, y) -> toInstruction instructionType [x, y]) coords |> String.join ""
+    List.map (\( x, y ) -> toInstruction instructionType [ x, y ]) coords |> String.join ""
 
 
 toInstruction : String -> List Float -> String
 toInstruction instructionType coords =
-  let
-    coordsString =
-      List.map toString coords
-      |> String.join ","
-  in
-    instructionType ++ " " ++ coordsString
+    let
+        coordsString =
+            List.map toString coords
+                |> String.join ","
+    in
+        instructionType ++ " " ++ coordsString
 
 
-startPath : List (Float, Float) -> (String, List (Float, Float))
+startPath : List ( Float, Float ) -> ( String, List ( Float, Float ) )
 startPath data =
-  let
-    (x, y) = Maybe.withDefault (0, 0) (List.head data)
-    tail = Maybe.withDefault [] (List.tail data)
-  in
-    (toInstruction "M" [x, y], tail)
+    let
+        ( x, y ) =
+            Maybe.withDefault ( 0, 0 ) (List.head data)
+
+        tail =
+            Maybe.withDefault [] (List.tail data)
+    in
+        ( toInstruction "M" [ x, y ], tail )
--- src/Plot.elm
+++ src/Plot.elm
@@ -4,231 +4,287 @@ import Html exposing (Html, button, div, text)
 import Svg exposing (g)
 import Svg.Attributes exposing (height, width, style, d)
 import String
-
-import Helpers exposing (
-  viewSvgContainer,
-  viewAxisPath,
-  viewSvgLine,
-  viewSvgText,
-  startPath,
-  toInstruction,
-  getLowest,
-  getHighest,
-  byPrecision,
-  coordToInstruction)
+import Helpers
+    exposing
+        ( viewSvgContainer
+        , viewAxisPath
+        , viewSvgLine
+        , viewSvgText
+        , startPath
+        , toInstruction
+        , getLowest
+        , getHighest
+        , byPrecision
+        , coordToInstruction
+        )
 
 
 -- TYPES
 
-type SerieType = Line | Area
+
+type SerieType
+    = Line
+    | Area
+
 
 type alias SerieConfig data =
     { serieType : SerieType
     , color : String
     , areaColor : String
-    , toCoords : data -> List (Float, Float)
+    , toCoords : data -> List ( Float, Float )
     }
 
 
 type alias PlotConfig data =
-  { dimensions : (Int, Int)
-  , tickHeight : Int
-  , series : List (SerieConfig data)
-  }
+    { dimensions : ( Int, Int )
+    , tickHeight : Int
+    , series : List (SerieConfig data)
+    }
 
 
 type alias Coord =
-  (Float, Float)
+    ( Float, Float )
 
 
-type Axis = XAxis | YAxis
+type Axis
+    = XAxis
+    | YAxis
+
 
 
 -- TODO: Move to config
-totalTicks = 
-  (12, 6)
+
+
+totalTicks =
+    ( 12, 6 )
+
 
 
 -- VIEW
 
 
 type alias AxisProps =
-  { axis : Axis
-  , getValue : Coord -> Float
-  , lowestValue : Float
-  , highestValue : Float
-  , span : Float
-  , origin : Float
-  , toSvg : Float -> Float
-  }
+    { axis : Axis
+    , getValue : Coord -> Float
+    , lowestValue : Float
+    , highestValue : Float
+    , span : Float
+    , origin : Float
+    , toSvg : Float -> Float
+    }
 
 
 getAxisProps : Axis -> PlotConfig data -> List data -> AxisProps
 getAxisProps axis { dimensions, series } data =
-  let
-    getValue = 
-      case axis of
-        XAxis -> fst
-        YAxis -> snd
+    let
+        getValue =
+            case axis of
+                XAxis ->
+                    fst
 
-    values =
-      List.map2 .toCoords series data
-      |> List.concat
-      |> List.map getValue
+                YAxis ->
+                    snd
 
-    edgeValues = 
-      (getLowest values, getHighest values)
+        values =
+            List.map2 .toCoords series data
+                |> List.concat
+                |> List.map getValue
 
-    (lowestValue, highestValue) =
-      edgeValues
-  
-    span =
-      abs lowestValue + abs highestValue
+        edgeValues =
+            ( getLowest values, getHighest values )
 
-    delta =
-      (toFloat (getValue dimensions)) / span
+        ( lowestValue, highestValue ) =
+            edgeValues
 
-    origin =
-      abs (getValue edgeValues) * delta
+        span =
+            abs lowestValue + abs highestValue
 
-    toSvg =
-      case axis of
-        XAxis ->
-          (\x -> origin + delta * x)
-        YAxis ->
-          (\y -> origin - delta * y)
-  in
-    AxisProps
-      axis
-      getValue
-      lowestValue
-      highestValue
-      span
-      origin
-      toSvg
+        delta =
+            (toFloat (getValue dimensions)) / span
 
+        origin =
+            abs (getValue edgeValues) * delta
+
+        toSvg =
+            case axis of
+                XAxis ->
+                    (\x -> origin + delta * x)
+
+                YAxis ->
+                    (\y -> origin - delta * y)
+    in
+        AxisProps
+            axis
+            getValue
+            lowestValue
+            highestValue
+            span
+            origin
+            toSvg
 
 
 viewPlot : PlotConfig data -> List data -> Html msg
 viewPlot config data =
-  let
-    xAxis =
-      getAxisProps XAxis config data
+    let
+        xAxis =
+            getAxisProps XAxis config data
 
-    yAxis =
-      getAxisProps YAxis config data
+        yAxis =
+            getAxisProps YAxis config data
 
-    toSvgCoords =
-      (\(x, y) -> (xAxis.toSvg x, yAxis.toSvg y))
+        toSvgCoords =
+            (\( x, y ) -> ( xAxis.toSvg x, yAxis.toSvg y ))
 
-    axisPositionX = (0, yAxis.origin)
-    axisPositionY = (xAxis.origin, 0)
+        axisPositionX =
+            ( 0, yAxis.origin )
 
-  in
-    viewFrame config
-      [ Svg.g [] (List.map2 (viewSeries toSvgCoords) config.series data)
-      , viewAxis xAxis config axisPositionX 
-      , viewAxis yAxis config axisPositionY
-      ]
+        axisPositionY =
+            ( xAxis.origin, 0 )
+    in
+        viewFrame config
+            [ Svg.g [] (List.map2 (viewSeries toSvgCoords) config.series data)
+            , viewAxis xAxis config axisPositionX
+            , viewAxis yAxis config axisPositionY
+            ]
 
 
 viewFrame : PlotConfig data -> List (Svg.Svg a) -> Svg.Svg a
 viewFrame { dimensions } children =
-  let
-    (width, height) = dimensions
-  in
-    Svg.svg
-      [ Svg.Attributes.height (toString height)
-      , Svg.Attributes.width (toString width)
-      , style "padding: 50px;"
-      ]
-      children
+    let
+        ( width, height ) =
+            dimensions
+    in
+        Svg.svg
+            [ Svg.Attributes.height (toString height)
+            , Svg.Attributes.width (toString width)
+            , style "padding: 50px;"
+            ]
+            children
 
 
 viewAxis : AxisProps -> PlotConfig data -> Coord -> Svg.Svg a
 viewAxis { axis, getValue, span, lowestValue, toSvg } { tickHeight, dimensions } position =
-  let
-    tickDelta = 
-      toFloat (floor (span / (getValue totalTicks)))
+    let
+        tickDelta =
+            toFloat (floor (span / (getValue totalTicks)))
 
-    lowestTick =
-      byPrecision tickDelta ceiling lowestValue
+        lowestTick =
+            byPrecision tickDelta ceiling lowestValue
 
-    tickIndexes =
-      [0..(getValue totalTicks)]
+        tickIndexes =
+            [0..(getValue totalTicks)]
 
-    ticks =
-      List.map (\i -> (lowestTick + i * tickDelta)) tickIndexes 
+        ticks =
+            List.map (\i -> (lowestTick + i * tickDelta)) tickIndexes
 
-    toTickCoords =
-      case axis of
-        XAxis -> (\x -> (toSvg x, 0, 0, tickHeight))
-        YAxis -> (\y -> (0, toSvg y, -tickHeight, 0))
+        toTickCoords =
+            case axis of
+                XAxis ->
+                    (\x -> ( toSvg x, 0, 0, tickHeight ))
 
-    ticksView =
-      Svg.g [] (List.map (toTickCoords >> viewSvgLine) ticks)
+                YAxis ->
+                    (\y -> ( 0, toSvg y, -tickHeight, 0 ))
 
-    displacement =
-      if axis == XAxis then (0, 20) else (-20, 5)
+        ticksView =
+            Svg.g [] (List.map (toTickCoords >> viewSvgLine) ticks)
 
-    labelsView =
-      viewSvgContainer displacement
-        (List.map (\tick -> viewSvgText (toTickCoords tick) (toString tick)) ticks)
+        displacement =
+            if axis == XAxis then
+                ( 0, 20 )
+            else
+                ( -20, 5 )
 
-    (width, height) =
-      dimensions
+        labelsView =
+            viewSvgContainer displacement
+                (List.map (\tick -> viewSvgText (toTickCoords tick) (toString tick)) ticks)
 
-    axisPath =
-      case axis of
-        XAxis -> toInstruction "H" [toFloat width]
-        YAxis -> toInstruction "V" [toFloat height]
+        ( width, height ) =
+            dimensions
 
-  in
-    viewSvgContainer position
-      [ viewAxisPath axisPath
-      , ticksView
-      , labelsView
-      ]
+        axisPath =
+            case axis of
+                XAxis ->
+                    toInstruction "H" [ toFloat width ]
+
+                YAxis ->
+                    toInstruction "V" [ toFloat height ]
+    in
+        viewSvgContainer position
+            [ viewAxisPath axisPath
+            , ticksView
+            , labelsView
+            ]
 
 
 viewSeries : (Coord -> Coord) -> SerieConfig data -> data -> Svg.Svg a
 viewSeries toSvgCoords config data =
-  case config.serieType of
-    Line -> viewSeriesLine toSvgCoords config data
-    Area -> viewSeriesArea toSvgCoords config data
+    case config.serieType of
+        Line ->
+            viewSeriesLine toSvgCoords config data
+
+        Area ->
+            viewSeriesArea toSvgCoords config data
+
 
 
 {- Draw area series -}
+
+
 viewSeriesArea : (Coord -> Coord) -> SerieConfig data -> data -> Svg.Svg a
 viewSeriesArea toSvgCoords config data =
-  let
-    allCoords = config.toCoords data
-    range = List.map fst allCoords
-    (lowestX, highestX) = (getLowest range, getHighest range)
+    let
+        allCoords =
+            config.toCoords data
 
-    svgCoords = List.map toSvgCoords allCoords
-    (highestSvgX, originY) = toSvgCoords (highestX, 0)
-    (lowestSvgX, _) = toSvgCoords (lowestX, 0)
+        range =
+            List.map fst allCoords
 
-    startInstruction = toInstruction "M" [lowestSvgX, originY]
-    endInstructions = toInstruction "L" [highestSvgX, originY]
-    instructions = coordToInstruction "L" svgCoords
+        ( lowestX, highestX ) =
+            ( getLowest range, getHighest range )
 
-    style' =
-      String.join "" ["stroke: ", config.color, "; fill:", config.areaColor]
-  in
-    Svg.path
-      [ d (startInstruction ++ instructions ++ endInstructions ++ "Z"), style style' ]
-      []
+        svgCoords =
+            List.map toSvgCoords allCoords
+
+        ( highestSvgX, originY ) =
+            toSvgCoords ( highestX, 0 )
+
+        ( lowestSvgX, _ ) =
+            toSvgCoords ( lowestX, 0 )
+
+        startInstruction =
+            toInstruction "M" [ lowestSvgX, originY ]
+
+        endInstructions =
+            toInstruction "L" [ highestSvgX, originY ]
+
+        instructions =
+            coordToInstruction "L" svgCoords
+
+        style' =
+            String.join "" [ "stroke: ", config.color, "; fill:", config.areaColor ]
+    in
+        Svg.path
+            [ d (startInstruction ++ instructions ++ endInstructions ++ "Z"), style style' ]
+            []
+
 
 
 {- Draw line series -}
+
+
 viewSeriesLine : (Coord -> Coord) -> SerieConfig data -> data -> Svg.Svg a
 viewSeriesLine toSvgCoords config data =
-  let
-    svgCoords = List.map toSvgCoords (config.toCoords data)
-    (startInstruction, tail) = startPath svgCoords
-    instructions = coordToInstruction "L" svgCoords
-    style' = String.join "" ["stroke: ", config.color, "; fill: none;"]
-  in
-    Svg.path [ d (startInstruction ++ instructions), style style' ] []
+    let
+        svgCoords =
+            List.map toSvgCoords (config.toCoords data)
+
+        ( startInstruction, tail ) =
+            startPath svgCoords
+
+        instructions =
+            coordToInstruction "L" svgCoords
+
+        style' =
+            String.join "" [ "stroke: ", config.color, "; fill: none;" ]
+    in
+        Svg.path [ d (startInstruction ++ instructions), style style' ] []
