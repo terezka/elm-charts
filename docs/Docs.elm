82c5e99e20566319d1cea20e60b898658348dda6
Nik Graf <nik@nikgraf.com>
2016-10-28 13:57:00 +0200

    improve docs

--- docs/Docs.elm
+++ docs/Docs.elm
@@ -25,36 +25,36 @@ myCustomLabel tick =
         [ Svg.tspan [] [ Svg.text ((toString (round tick)) ++ " ms") ] ]
 
 
+areaData : List ( Float, Float )
 areaData =
     [ ( -50, 34 ), ( -30, 432 ), ( -20, 35 ), ( 2, 546 ), ( 10, 345 ), ( 30, -42 ), ( 90, 67 ), ( 120, 50 ) ]
 
 
+lineData : List ( Float, Float )
 lineData =
     [ ( -50, 34 ), ( -30, 32 ), ( -20, 5 ), ( 2, -46 ), ( 10, -99 ), ( 30, -136 ), ( 90, -67 ), ( 120, 10 ) ]
 
+firstExampleAreaData : List ( Float, Float )
+firstExampleAreaData =
+    [ ( 0, 12 ), ( 1, 15 ), ( 2, 16 ), ( 3, 14 ), ( 4, 11 ), ( 5, 13 ), ( 6, 20 ), ( 7, 24 ) ]
 
 main =
-    div [ style [ ( "width", "800px" )
-                , ( "margin", "80px auto" )
+    div [ style [ ( "width", "600px" )
+                , ( "padding", "80px auto" )
                 , ( "font-family", "sans-serif" )
                 , ( "color", "#7F7F7F" )
                 , ( "text-align", "center" ) ] ]
         [ img [ src "logo.png", style [ ( "width", "100px" ), ( "height", "100px" ) ] ] []
         , h1 [ style [ ( "font-weight", "200" ) ] ] [ text "Elm Plot" ]
         , plot
-            [ dimensions ( 800, 500 ) ]
-            [ horizontalGrid [ gridTicks (Just [ -40, -20, 20, 40, 60, 80, 100 ]), gridStyle [ ( "stroke", "#cee0e2" ) ] ]
-            , verticalGrid [ gridTicks (Just [ 200, 400, 600 ]), gridStyle [ ( "stroke", "#cee0e2" ) ] ]
-            , area [ stroke "cornflowerblue", fill "#ccdeff" ] areaData
-            , line [ stroke "mediumvioletred" ] lineData
-            , xAxis
-                [ customViewTick myCustomXTick
-                , axisLineStyle [ ( "stroke", "red" ) ]
-                , tickList [ -20, 20, 40, 82 ]
-                ]
-            , yAxis
-                [ customViewLabel myCustomLabel
-                , amountOfTicks 5
-                ]
-            ]
+          [ dimensions ( 600, 250 ) ]
+          [ area [ stroke "6A6A6A", fill "#ccdeff" ] firstExampleAreaData
+          , xAxis
+              [ axisLineStyle [ ( "stroke", "#7F7F7F" ) ]
+              , tickList [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+              ]
+          , yAxis
+              [ amountOfTicks 6
+              ]
+          ]
         ]
