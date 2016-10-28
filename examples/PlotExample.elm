595c46d2977ef4fab59e4c5094917fdb7b72d130
Tereza Sokol <terezasokol@gmail.com>
2016-10-28 20:20:28 +0200

    Update example

--- examples/PlotExample.elm
+++ examples/PlotExample.elm
@@ -3,59 +3,120 @@ module PlotExample exposing (..)
 import Svg
 import Svg.Attributes
 import Plot exposing (..)
+import Debug
 
 
-myCustomXTick : Float -> Svg.Svg a
-myCustomXTick tick =
-    Svg.text'
-        [ Svg.Attributes.transform "translate(0, 8)"
-        , Svg.Attributes.style "text-anchor: middle;"
-        ]
-        [ Svg.tspan [] [ Svg.text "⚡️" ] ]
+isOdd : Float -> Bool
+isOdd n =
+    rem (round (abs n)) 2 > 0
 
 
-myCustomLabel : Float -> Svg.Svg a
-myCustomLabel tick =
-    Svg.text'
-        [ Svg.Attributes.transform "translate(-10, 4)"
-        , Svg.Attributes.style "stroke: purple; text-anchor: end;"
-        ]
-        [ Svg.tspan [] [ Svg.text ((toString tick) ++ " ms") ] ]
+type Orientation
+    = X
+    | Y
 
 
-areaData : List ( Float, Float )
-areaData =
-    [ ( -50, 34 ), ( -30, 432 ), ( -20, 35 ), ( 2, 546 ), ( 10, 345 ), ( 30, -42 ), ( 90, 67 ), ( 120, 50 ) ]
+customTick : Float -> Svg.Svg a
+customTick tick =
+    let
+        length =
+            if isOdd tick then
+                7
+            else
+                10
 
-
-lineData : List ( Float, Float )
-lineData =
-    [ ( -50, 34 ), ( -30, 32 ), ( -20, 5 ), ( 2, -46 ), ( 10, -99 ), ( 30, -136 ), ( 90, -67 ), ( 120, 10 ) ]
-
-
-example1 =
-    plot
-        [ size ( 800, 500 ) ]
-        [ horizontalGrid [ gridTickList [ -40, -20, 20, 40, 60, 80, 100 ], gridStyle [ ( "stroke", "#cee0e2" ) ] ]
-        , verticalGrid [ gridTickList [ 200, 400, 600 ], gridStyle [ ( "stroke", "#cee0e2" ) ] ]
-        , area [ stroke "cornflowerblue", fill "#ccdeff" ] areaData
-        , line [ stroke "mediumvioletred" ] lineData
-        , xAxis
-            [ customViewTick myCustomXTick
-            , axisLineStyle [ ( "stroke", "red" ) ]
-            , tickList [ -20, 20, 40, 82 ]
+        color =
+            if isOdd tick then
+                "#e4e3e3"
+            else
+                "#b9b9b9"
+    in
+        Svg.line
+            [ Svg.Attributes.style ("stroke: " ++ color)
+            , Svg.Attributes.y2 (toString length)
             ]
-        , yAxis
-            [ customViewLabel myCustomLabel
-            , stepSize 5
+            []
+
+
+formatTickX : Float -> String
+formatTickX tick =
+    if tick == 0 then
+        ""
+    else if isOdd tick then
+        ""
+    else
+        let
+            abbrivated =
+                (abs tick * 2) > 10 
+
+            formatted =
+                if abbrivated then tick / 10 * 2 else tick * 200
+
+            abbrivation =
+                if abbrivated then "k t" else " t"
+        in
+            (toString formatted) ++ abbrivation
+
+
+formatTickY : Float -> String
+formatTickY tick =
+    if tick == 0 then
+        ""
+    else
+        (toString tick) ++ " °C"
+
+
+customLabel : Orientation -> Float -> Svg.Svg a
+customLabel orientation tick =
+    let
+        length =
+            if isOdd tick then
+                7
+            else
+                10
+
+        text =
+            if orientation == X then
+                formatTickX tick
+            else
+                formatTickY tick
+
+        style =
+            if orientation == X then
+                "text-anchor: middle;"
+            else
+                "text-anchor: end;"
+
+        displacement =
+            if orientation == X then
+                "0, 27"
+            else
+                "-10, 5"
+    in
+        Svg.text'
+            [ Svg.Attributes.transform ("translate(" ++ displacement ++ ")")
+            , Svg.Attributes.style (style ++ " stroke: #969696; font-size: 12px;")
             ]
-        ]
+            [ Svg.tspan [] [ Svg.text text ] ]
 
 
+data1 : List ( Float, Float )
+data1 =
+    [ ( -10, 14 ), ( -9, 5 ), ( -8, -9 ), ( -7, -15 ), ( -6, -22 ), ( -5, -12 ), ( -4, -8 ), ( -3, -1 ), ( -2, 6 ), ( -1, 10 ), ( 0, 14 ), ( 1, 16 ), ( 2, 26 ), ( 3, 32 ), ( 4, 28 ), ( 5, 32 ), ( 6, 29 ), ( 7, 46 ), ( 8, 52 ), ( 9, 53 ), ( 10, 59 ) ]
+
+
+data2 : List ( Float, Float )
+data2 =
+    [ ( -10, 34 ), ( -9, 38 ), ( -8, 40 ), ( -7, 41 ), ( -6, 50 ), ( -5, 52 ), ( -4, 53 ), ( -3, 49 ), ( -2, 42 ), ( -1, 52 ), ( -0.5, 53 ), ( 0.5, 46 ), ( 1, 40 ), ( 2, 36 ), ( 3, 31 ), ( 4, 25 ), ( 5, 29 ), ( 6, 37 ), ( 7, 43 ), ( 8, 48 ), ( 9, 58 ), ( 10, 64 ) ]
+
+
+main : Svg.Svg a
 main =
     plot
-        [ size ( 800, 500 ) ]
-        [ line [ stroke "mediumvioletred" ] lineData
-        , xAxis [ stepSize 10 ]
-        , yAxis [ amountOfTicks 5 ]
+        [ size ( 600, 250 ), padding ( 40, 60 ) ]
+        [ verticalGrid [ gridTickList [ -30, 30, 70 ], gridStyle [ ( "stroke", "#e2e2e2" ) ] ]
+        , area [ areaStyle [ ( "stroke", "#e6d7ce" ), ( "fill", "#feefe5" ) ] ] data1
+        , line [ lineStyle [ ( "stroke", "#b6c9ef" ) ] ] data2
+        , yAxis [ axisLineStyle [ ( "stroke", "#b9b9b9" ) ], stepSize 20, customViewLabel (customLabel Y) ]
+        , xAxis [ axisLineStyle [ ( "stroke", "#b9b9b9" ) ], customViewTick customTick, customViewLabel (customLabel X) ]
         ]
