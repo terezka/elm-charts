ba7291682e932a39e499475500523ed22f88fb7a
Nik Graf <nik@nikgraf.com>
2016-10-28 15:34:00 +0200

    move examples to separate files

---
+++ docs/AreaChart.elm
@@ -0,0 +1,20 @@
+module AreaChart exposing (areaChart)
+
+import Svg
+import Svg.Attributes
+import Plot exposing (..)
+
+data : List ( Float, Float )
+data =
+    [ ( 0, 8 ), ( 1, 13 ), ( 2, 14 ), ( 3, 12 ), ( 4, 11 ), ( 5, 16 ), ( 6, 22 ), ( 7, 32 ) ]
+
+areaChart : Svg.Svg a
+areaChart =
+  plot
+    [ size ( 600, 250 ) ]
+    [ area [ serieStyle [ ( "stroke", "#cfd8ea" ), ( "fill", "#e4eeff") ] ] data
+    , xAxis
+        [ axisLineStyle [ ( "stroke", "#7F7F7F" ) ]
+        , tickList [ 0, 1, 2, 3, 4, 5, 6, 7 ]
+        ]
+    ]
