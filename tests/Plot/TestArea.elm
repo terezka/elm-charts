a2b6ef841fa51539b4f8a637d067aebc2356289f
Tereza Sokol <terezasokol@gmail.com>
2017-01-14 18:33:35 +0100

    Area test: add smoothing to config

--- tests/Plot/TestArea.elm
+++ tests/Plot/TestArea.elm
@@ -3,21 +3,28 @@ module Plot.TestArea exposing (all)
 import Test exposing (..)
 import Expect
 import Svg.Attributes
+import Internal.Types exposing (..)
+import Internal.Area exposing (..)
 import Plot.Area exposing (..)
+
 
 all : Test
 all =
-  describe "Plot.Area"
-      [ testStroke
-      , testStrokeWidth
-      , testFill
-      , testOpacity
-      , testCustomAttrs
-      ]
+    describe "Plot.Area"
+        [ testStroke
+        , testStrokeWidth
+        , testFill
+        , testOpacity
+        , testCustomAttrs
+        ]
 
 
+testConfig : Config msg
 testConfig =
-    { style = [ ( "color", "green" ) ], customAttrs = [] }
+    { style = [ ( "color", "green" ) ]
+    , customAttrs = []
+    , smoothing = None
+    }
 
 
 testStroke : Test
@@ -27,8 +34,12 @@ testStroke =
             \() ->
                 Expect.equal
                     (stroke "black" testConfig)
-                    { style = [ ( "stroke", "black" ), ( "color", "green" ) ], customAttrs = [] }
+                    { style = [ ( "stroke", "black" ), ( "color", "green" ) ]
+                    , customAttrs = []
+                    , smoothing = None
+                    }
         ]
+
 
 testStrokeWidth : Test
 testStrokeWidth =
@@ -37,7 +48,10 @@ testStrokeWidth =
             \() ->
                 Expect.equal
                     (strokeWidth 2 testConfig)
-                    { style = [ ( "stroke-width", "2px" ), ( "color", "green" ) ], customAttrs = [] }
+                    { style = [ ( "stroke-width", "2px" ), ( "color", "green" ) ]
+                    , customAttrs = []
+                    , smoothing = None
+                    }
         ]
 
 
@@ -45,10 +59,13 @@ testFill : Test
 testFill =
     describe "fill"
         [ test "should update the config with the new fill color" <|
-              \() ->
-                  Expect.equal
-                      (fill "red" testConfig)
-                        { style = [ ( "fill", "red" ), ( "color", "green" ) ], customAttrs = [] }
+            \() ->
+                Expect.equal
+                    (fill "red" testConfig)
+                    { style = [ ( "fill", "red" ), ( "color", "green" ) ]
+                    , customAttrs = []
+                    , smoothing = None
+                    }
         ]
 
 
@@ -56,10 +73,13 @@ testOpacity : Test
 testOpacity =
     describe "opacity"
         [ test "should update the config with new opacity value" <|
-              \() ->
-                  Expect.equal
-                      (opacity 0.8 testConfig)
-                      { style = [ ( "opacity", "0.8" ), ( "color", "green" ) ], customAttrs = [] }
+            \() ->
+                Expect.equal
+                    (opacity 0.8 testConfig)
+                    { style = [ ( "opacity", "0.8" ), ( "color", "green" ) ]
+                    , customAttrs = []
+                    , smoothing = None
+                    }
         ]
 
 
@@ -67,8 +87,11 @@ testCustomAttrs : Test
 testCustomAttrs =
     describe "customAttrs"
         [ test "should update config with the new custom attributes" <|
-              \() ->
-                  Expect.equal
-                      (customAttrs [ Svg.Attributes.dx "10px" ] testConfig )
-                      { style = [ ( "color", "green" ) ], customAttrs = [ Svg.Attributes.dx "10px" ] }
+            \() ->
+                Expect.equal
+                    (customAttrs [ Svg.Attributes.dx "10px" ] testConfig)
+                    { style = [ ( "color", "green" ) ]
+                    , customAttrs = [ Svg.Attributes.dx "10px" ]
+                    , smoothing = None
+                    }
         ]
