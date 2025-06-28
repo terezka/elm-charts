064089f0751c6fd07d08561852196b59caa660fc
Tereza Sokol <terezasokol@gmail.com>
2025-06-28 21:40:39 +0200

    Fix copy script

--- copy-to-alpha.sh
+++ copy-to-alpha.sh
@@ -9,6 +9,6 @@ cp ./elm.json ../elm-charts-alpha/elm.json
 
 vim ../elm-charts-alpha/elm.json
 
-echo -e "# Elm Charts Alpha\n\nThis is the alpha version of elm-charts. Use at own risk.\n" > ../elm-charts-alpha/README.md
-echo -e "-------------\n" >> ../elm-charts-alpha/README.md
+echo "# Elm Charts Alpha\n\nThis is the alpha version of elm-charts. Use at own risk.\n" > ../elm-charts-alpha/README.md
+echo "-------------\n" >> ../elm-charts-alpha/README.md
 cat ./README.md >> ../elm-charts-alpha/README.md
